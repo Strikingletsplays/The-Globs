5d0b0cfab0a68eab07bebc98c7d2febeda650a9b
StrikingLetsPlays <007makiz@gmail.com>
2020-02-27 20:59:27 +0200

    Major Update ;)

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public float speed;
+    public float FollowRange;
+    public float stopRange;
+    private Transform Player;
+
+    //For checking if glob is gounded
+    public float jumpForce;
+    private bool m_Grounded;
+    const float k_GroundedRadius = .3f; // Radius of the overlap circle to determine if grounded
+    [SerializeField] private Transform m_GroundCheck = null;
+    [SerializeField] public LayerMask m_WhatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    void FixedUpdate()
+    {
+        m_Grounded = false;
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+            }
+        }
+        if (Vector2.Distance(transform.position, Player.position) < FollowRange)
+        {
+            if (!(Vector2.Distance(transform.position, Player.position) < stopRange))
+            {
+                transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
+                if ((Player.position.y - this.transform.position.y) > 1 && m_Grounded)
+                {
+                    //jump
+                    this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+                }
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/pickup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class pickup : MonoBehaviour
+{
+    public Transform theDest;
+    private Transform player;
+    private bool isHolding;
+
+    void Start()
+    {
+        isHolding = false;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        if (isHolding)
+        {
+            this.GetComponent<Rigidbody2D>().simulated = false;
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            this.GetComponent<Rigidbody2D>().angularVelocity = 0;
+        }
+        
+    }
+    void OnMouseDown()
+    {
+        //Debug.Log(player.position.x - me.transform.position.x);
+        if (((player.position.x - this.transform.position.x) < 1.5 && (player.position.y - this.transform.position.y) < 1))
+        {
+            isHolding = true;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            this.gameObject.transform.position = theDest.position;
+            this.transform.parent = GameObject.Find("Destination").transform;
+        }
+    }
+    void OnMouseUp()
+    {
+        isHolding = false;
+        this.transform.parent = null;
+        GetComponent<Rigidbody2D>().gravityScale = 3;
+        this.GetComponent<Rigidbody2D>().simulated = true;
+    }
+
+}
