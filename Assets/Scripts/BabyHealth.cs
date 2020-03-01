9adb73a560af71e029759f2c357046928af654ad
StrikingLetsPlays <007makiz@gmail.com>
2020-03-01 14:42:22 +0200

    Added background + fixed edges + added BG UI Health

--- Assets/Scripts/BabyHealth.cs
+++ Assets/Scripts/BabyHealth.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class BabyHealth : MonoBehaviour
 {
     public int Health = 1;
+    public int MaxHealth = 4;
     private Vector3 ScaleChangeSize;
+    public HealthBar HealthBar;
 
+    private void Start()
+    {
+        HealthBar.SetMaxHealth(MaxHealth);
+    }
     // Update is called once per frame
     void Update()
     {
+        HealthBar.SetHealth(Health);
         if (Health <= 0)
         {
             //kill baby
@@ -22,10 +29,11 @@ public class BabyHealth : MonoBehaviour
     }
     public void increceHealth()
     {
-        if(Health <= 4) {
+        if(Health <= 3) {
             Health++;
+            HealthBar.SetHealth(Health);
             //increase scale
-            if(this.gameObject.GetComponent<Transform>().localScale.x > 0) {
+            if (this.gameObject.GetComponent<Transform>().localScale.x > 0) {
                 ScaleChangeSize = new Vector3(0.1f, 0.1f, 0.1f);
                 this.transform.localScale += ScaleChangeSize;
             }
@@ -37,17 +45,16 @@ public class BabyHealth : MonoBehaviour
     }
     public void TakeDamage()
     {
-            Health--;
-            //decrese scale
-            if (this.gameObject.GetComponent<Transform>().localScale.x > 0)
-            {
-                ScaleChangeSize = new Vector3(-0.1f, -0.1f, -0.1f);
-                this.transform.localScale += ScaleChangeSize;
-            }
-            else
-            {
-                ScaleChangeSize = new Vector3(0.1f, -0.1f, -0.1f);
-                this.transform.localScale += ScaleChangeSize;
-            }
+        Health--;
+        HealthBar.SetHealth(Health);
+        //decrese scale
+        if (this.gameObject.GetComponent<Transform>().localScale.x > 0)
+         {
+            ScaleChangeSize = new Vector3(-0.1f, -0.1f, -0.1f);
+            this.transform.localScale += ScaleChangeSize;
+        }else{
+            ScaleChangeSize = new Vector3(0.1f, -0.1f, -0.1f);
+            this.transform.localScale += ScaleChangeSize;
+        }
     }
 }
