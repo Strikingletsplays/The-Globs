2443ec8c032cc228cdbaab8562fb814cc9cd872c
StrikingLetsPlays <007makiz@gmail.com>
2020-03-06 18:05:49 +0200

    temp

---
+++ Assets/Scripts/Enemy_Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Move : MonoBehaviour
+{
+    private Vector3 PlayerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+    public float Speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.x - PlayerPos.x < 2)
+        {
+            transform.position = transform.forward * Speed * Time.deltaTime;
+        }
+    }
+}
