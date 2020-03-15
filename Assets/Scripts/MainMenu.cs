15badff96cd7a0b13925aa33d49a582f6d4eeaac
StrikingLetsPlays <007makiz@gmail.com>
2020-03-16 00:19:03 +0200

    Main Menu + Pause Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetLevel(float SliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(SliderValue) * 20);
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public static bool GameIsPaused = false;
+    public GameObject Camera;
+    public GameObject PauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        //Camera.GetComponent<AudioSource>().volume = 0.2f;
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        //Camera.GetComponent<AudioSource>().volume = 0.05f;
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void SetLevel(float SliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(SliderValue) * 20);
+    }
+}
