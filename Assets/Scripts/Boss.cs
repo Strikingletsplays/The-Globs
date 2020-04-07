09ffca90345fcf498affdc2557b700e3202a2cc5
StrikingLetsPlays <007makiz@gmail.com>
2020-04-07 19:02:11 +0300

    enemy boss + layers add + animations + unity update

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private Animator BossAnim;
+    private Transform PlayerPos;
+    public float timeBetweenAtack = 1.5f;
+    public float Speed = 2;
+    public float MinDistance = 6;
+    private void Start()
+    {
+        BossAnim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Find and chase player
+        PlayerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        if (Vector2.Distance(transform.position, PlayerPos.position) < MinDistance)
+        {
+            if (!(Vector2.Distance(transform.position, PlayerPos.position) < 1))
+            {
+                RotateEnemyToPlayer();
+                BossAnim.SetBool("isMoving", true);
+                transform.position = Vector2.MoveTowards(transform.position, PlayerPos.position, Speed * Time.deltaTime);
+            }
+            else 
+            { 
+                transform.position = transform.position;
+            }
+        }
+        else 
+        { 
+            BossAnim.SetBool("isMoving", false);
+        }
+        //if Atacking
+        if (Vector2.Distance(transform.position, PlayerPos.position) < 1.5)
+        {
+            BossAnim.SetBool("isAtacking", true);
+        }
+        else 
+        {
+            BossAnim.SetBool("isAtacking", false);
+        }
+    }
+    void RotateEnemyToPlayer()
+    {
+        //Rotating Enemy to face player
+        if (transform.position.x - PlayerPos.position.x < 0)
+        {
+            transform.localScale = new Vector3(0.59929f, 0.59929f, 0.59929f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-0.59929f, 0.59929f, 0.59929f);
+        }
+    }
+}
--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "ParentGlob")
+        if (collision.tag == "Player")
         {
-            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage();
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 400.0f);
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.forward * 400.0f);
+            collision.GetComponent<PlayerHealth>().TakeDamage();
+            collision.GetComponent<Rigidbody2D>().AddForce(transform.up * 400.0f);
+            collision.GetComponent<Rigidbody2D>().AddForce(transform.forward * 400.0f);
         }
-        if (collision.gameObject.tag == "BabyGlob")
+        if (collision.tag == "BabyGlob")
         {
-            collision.gameObject.GetComponent<BabyHealth>().TakeDamage();
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 400.0f);
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.forward * 400.0f);
+            collision.GetComponent<BabyHealth>().TakeDamage();
+            collision.GetComponent<Rigidbody2D>().AddForce(transform.up * 400.0f);
+            collision.GetComponent<Rigidbody2D>().AddForce(transform.forward * 400.0f);
         }
     }
 }
